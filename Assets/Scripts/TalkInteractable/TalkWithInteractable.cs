246d0525405c82e38f8c333875547f80a665f743
charoodle <60276941+charoodle@users.noreply.github.com>
2024-11-18 17:56:14 -0800

    Dialogue: Added abstract TalkInteractable class and basic player+npc scene.

---
+++ Assets/Scripts/TalkInteractable/TalkWithInteractable.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TalkWithInteractable : Interactable
+{
+    [SerializeField] protected string _npcName;
+    protected string npcName
+    {
+        get { return _npcName; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                Debug.LogError("NPC Name cannot be empty.");
+                return;
+            }
+            _npcName = value;
+        }
+    }
+
+    public override void InteractWith()
+    {
+        base.InteractWith();
+
+        TalkWith();
+    }
+
+    protected virtual void TalkWith()
+    {
+        Debug.Log("Talk with: " + npcName);
+        StartCoroutine(TalkWithCoroutine());
+    }
+
+    protected abstract IEnumerator TalkWithCoroutine();
+}
---
+++ Assets/Scripts/TalkInteractable/TalkWith_Henry.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkWith_Henry : TalkWithInteractable
+{
+    protected override IEnumerator TalkWithCoroutine()
+    {
+        throw new System.NotImplementedException();
+    }
+}
